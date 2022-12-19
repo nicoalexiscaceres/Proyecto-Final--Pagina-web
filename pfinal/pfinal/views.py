4af947dffabdc45b276402f082ecb85b6f0bc12b
nicoalexiscaceres <nicoalexiscaceres@gmail.com>
2022-12-19 19:12:41 -0300

    cree un una estructura con bootstrap

--- pfinal/pfinal/views.py
+++ pfinal/pfinal/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 def inicio(request):
     template_name= "index.html"
     contexto={
+        'Nombre':"Nico"
     }
     return render(request,template_name,contexto)
